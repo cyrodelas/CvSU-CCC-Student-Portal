6c05114d437824cb89c562b9e57aeed09472c9d0
cyrodelas <cyrodelasluna@gmail.com>
2020-06-03 13:40:31 +0800

    Updated codes

--- application/config/database.php
+++ application/config/database.php
@@ -96,25 +96,4 @@ $db['default'] = array(
 );
 
 
-$db['cvsu_hr'] = array(
-    'dsn'	=> '',
-    'hostname' => 'localhost',
-    'username' => 'root',
-    'password' => '',
-    'database' => 'cvsu_hr',
-    'dbdriver' => 'mysqli',
-    'dbprefix' => '',
-    'pconnect' => FALSE,
-    'db_debug' => (ENVIRONMENT !== 'production'),
-    'cache_on' => FALSE,
-    'cachedir' => '',
-    'char_set' => 'utf8',
-    'dbcollat' => 'utf8_general_ci',
-    'swap_pre' => '',
-    'encrypt' => FALSE,
-    'compress' => FALSE,
-    'stricton' => FALSE,
-    'failover' => array(),
-    'save_queries' => TRUE
-);
 
